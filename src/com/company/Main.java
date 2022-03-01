cbbdaed36be61d6e191ff0ed465f10d17749dfc1
RS212418 <roman.r.siva@gmail.com>
2022-03-01 15:13:04 +0000

    Added email verification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Roman");
+        String Email = "moo@gmail.com";
+        System.out.println(ValidateEmail(Email));
+    }
+
+
+    public static boolean ValidateEmail(String input){
+        String EmailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern EmailPattern = Pattern.compile(EmailRegex,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = EmailPattern.matcher(input);
+        return matcher.find();
+
     }
 }
