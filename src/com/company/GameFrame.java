5847788d4bb6a9ebd31197fcbe9ceee7ab2ded92
CallumPringle <pringcal@hotmail.com>
2022-03-22 14:50:35 +0000

    login and register are working and fixed but not sure how the email is supposed to be implemented, the window now pops to the front when run with some very messy code so try not delete anything :)

--- src/com/company/GameFrame.java
+++ src/com/company/GameFrame.java
@@ -3,7 +3,15 @@ import javax.swing.JFrame;
 
     public class GameFrame extends JFrame {
 
+        public @Override void toFront() {
+            int sta = super.getExtendedState() & ~JFrame.ICONIFIED & JFrame.NORMAL;
 
+            super.setExtendedState(sta);
+            super.setAlwaysOnTop(true);
+            super.toFront();
+            super.requestFocus();
+            super.setAlwaysOnTop(false);
+        }
         GameFrame() {
             GamePanel panel = new GamePanel();  //declare a new panel
             this.add(panel); //adding the panel we declared to our container
@@ -13,6 +21,10 @@ import javax.swing.JFrame;
             this.toFront();
             this.requestFocus();
             this.repaint();
+            setVisible(true);
+            toFront();
+            requestFocus();
+            repaint();
             this.setResizable(false); //we set this resizable function to false that will resist to make our panel resizable as we cannot just resize our panel by dragging it with mouse
             this.pack(); //pack our every components and layouts in minimum space
             this.setLocationRelativeTo(null); //sets the location of our panel on window
