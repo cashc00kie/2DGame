1399a42bf20adc3b05a3157e9ff40e13bc21714a
CallumPringle <pringcal@hotmail.com>
2022-03-22 13:54:05 +0000

    login and register are working and fixed but not sure how the email is supposed to be implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
@@ -13,12 +16,29 @@ public class Main {
 
     public static void main(String[] args) {
         createFile();
-        ConfirmLogin();
+        loginRegister();
         //Leaderboard();
         String Email = "moo@gmail.com";
         System.out.println(ValidateEmail(Email));
     }
-
+    public static String getInput(String prompt){
+        System.out.println(prompt);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+    public static void loginRegister(){
+        String userChoice = getInput("would you like to login or register?");
+        if (Objects.equals(userChoice, "login")){
+            Login();
+        }
+        else if (Objects.equals(userChoice, "register")){
+            Register();
+        }
+        else{
+            System.out.println("not a valid option");
+            loginRegister();
+        }
+    }
     public static void createFile(){
 
         try {
@@ -41,30 +61,7 @@ public class Main {
         return matcher.find();
 
     }
-
-    public static void ConfirmLogin() {
-
-        System.out.println("Would you like to login or register?");
-        Scanner input = new Scanner(System.in);
-        while (true) {
-            String userInput = input.nextLine();
-            if (userInput.equals("login")) {
-                VerifyLogin();
-                break;
-
-            }
-            if (userInput.equals("register")) {
-                //Register();
-                break;
-
-            } else {
-                System.out.println("Error");
-            }
-        }
-    }
-
-
-    public static void VerifyLogin() {
+    public static void Login() {
 
         Scanner Login = new Scanner(System.in);
         System.out.println("Enter your username: ");
@@ -104,26 +101,27 @@ public class Main {
 
 
 
-//    public static void Register() {
-//
-//        Scanner register = new Scanner(System.in);
-//        System.out.println("Enter your name");
-//        String username = register.next();
-//        System.out.println("Please enter your password");
-//        String password = register.next();
-//        File file = new File("LoginFile.txt");
-//        String userName = (username);
-//        FileWriter writer = null;
-//        try {
-//            writer = new FileWriter("LoginFile.txt", true);
-//            BufferedWriter bw = new BufferedWriter(writer);
-//            writer.write(userName + password);
-//            writer.close();
-//            System.out.println("Successfully registered to the library, hello " + username);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+   public static void Register() {
+
+        Scanner register = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String username = register.next();
+        System.out.println("Please enter your password");
+        String password = register.next();
+        File file = new File("LoginFile.txt");
+        String userName = (username);
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("LoginFile.txt", true);
+            BufferedWriter bw = new BufferedWriter(writer);
+            writer.write("\n" + userName + "," + password);
+            writer.close();
+            System.out.println("Successfully registered to the library, hello " + username);
+            loginRegister();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+   }
     }
 
 
